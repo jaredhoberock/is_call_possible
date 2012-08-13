785c3858b318106f5215e949a9c4655a8a3fe55e
Jared Hoberock <jaredhoberock@gmail.com>
2012-08-13 12:14:57 -0700

    Fix several problems with DEFINE_HAS_MEMBER. Add a couple of test cases to demo.cpp.

--- demo.cpp
+++ demo.cpp
@@ -1,54 +1,73 @@
 #include "is_call_possible.hpp"
 #include <cassert>
 #include <string>
+#include <vector>
+#include <list>
 #include <iostream>
 
 // define a trait to check for the possibility of calling operator() on a given type
 DEFINE_IS_CALL_POSSIBLE(is_call_possible, operator())
 
+// define a trait to check for the possibility of calling baz on a given type
+DEFINE_IS_CALL_POSSIBLE(is_baz_possible, baz);
+
 // test cases
-struct Foo 
+struct foo 
 { 
    void operator()(double) const {} 
    void operator()(std::string) const {} 
 }; 
 
-struct Bar : Foo
+struct bar : foo
 {
    int operator()(int)
    {
      return 0;
    }
+
+   float baz(std::vector<int>)
+   {
+     return 13;
+   }
 };
 
 int main() 
 { 
-  // Foo has operator() which takes a double and returns void
-  assert((is_call_possible<Foo, void(double)>::value)); 
+  // foo has operator() which takes a double and returns void
+  assert((is_call_possible<foo, void(double)>::value)); 
 
-  // Foo has operator() which takes int and returns void
-  assert((is_call_possible<Foo, void(int)>::value)); 
+  // foo has operator() which takes int and returns void
+  assert((is_call_possible<foo, void(int)>::value)); 
 
-  // Foo has operator() which can convert const char * to string and return void
-  assert((is_call_possible<Foo, void(const char *)>::value)); 
+  // foo has operator() which can convert const char * to string and return void
+  assert((is_call_possible<foo, void(const char *)>::value)); 
 
-  // Foo has no operator() which can take a void *
-  assert((!is_call_possible<Foo, void(void *)>::value)); 
+  // foo has no operator() which can take a void *
+  assert((!is_call_possible<foo, void(void *)>::value)); 
   
-  // Bar inherits Foo's operator() which takes a double and returns void
-  assert((is_call_possible<Bar, void(double)>::value)); 
+  // bar inherits foo's operator() which takes a double and returns void
+  assert((is_call_possible<bar, void(double)>::value)); 
 
-  // Bar has operator() which takes int and converts its int result to void
-  assert((is_call_possible<Bar, void(int)>::value)); 
+  // bar has operator() which takes int and converts its int result to void
+  assert((is_call_possible<bar, void(int)>::value)); 
 
-  // Bar has operator() which takes int but cannot convert const char * to int
-  assert((!is_call_possible<Bar, void(const char *)>::value)); 
+  // bar has operator() which takes int but cannot convert const char * to int
+  assert((!is_call_possible<bar, void(const char *)>::value)); 
 
-  // Neither Bar nor Foo has an operator() which can take void *
-  assert((!is_call_possible<Bar, void(void *)>::value)); 
+  // neither bar nor foo has an operator() which can take void *
+  assert((!is_call_possible<bar, void(void *)>::value)); 
 
-  // Bar has operator() which takes int and returns int
-  assert((is_call_possible<Bar, int(int)>::value)); 
+  // bar has operator() which takes int and returns int
+  assert((is_call_possible<bar, int(int)>::value)); 
+
+  // foo has no baz member function
+  assert((!is_baz_possible<foo, float(std::vector<int>)>::value));
+
+  // bar has member function baz which takes std::vector<int> and returns float
+  assert((is_baz_possible<bar, float(std::vector<int>)>::value));
+
+  // bar::baz can't be called with std::list<int>
+  assert((!is_baz_possible<bar, float(std::list<int>)>::value));
   
   return 0;
 } 
--- is_call_possible.hpp
+++ is_call_possible.hpp
@@ -3,24 +3,98 @@
 // inspired by Roman Perepelitsa's presentation from comp.lang.c++.moderated
 // based on the implementation here: http://www.rsdn.ru/forum/cpp/2759773.1.aspx
 
-#define DEFINE_HAS_MEMBER_FUNCTION(trait_name, member_function_name)          \
-template <typename T>                                                         \
-class trait_name                                                              \
-{                                                                             \
-   class yes { char m; };                                                     \
-   class no { yes m[2]; };                                                    \
-   struct base_mixin                                                          \
-   {                                                                          \
-     void member_function_name();                                             \
-   };                                                                         \
-   struct base : public T, public base_mixin {};                              \
-   template <typename U, U t>  class helper{};                                \
-   template <typename U>                                                      \
-   static no deduce(U*, helper<void (base_mixin::*)(), &U::operator()>* = 0); \
-   template <typename U> static yes deduce(...);                              \
-public:                                                                       \
-   static const bool value = sizeof(yes) == sizeof(deduce<T>(0));             \
-}; 
+#define DEFINE_HAS_MEMBER_FUNCTION(trait_name, member_function_name)                                         \
+template<typename T, typename Signature> class trait_name;                                                   \
+                                                                                                             \
+template<typename T, typename Result>                                                                        \
+class trait_name<T, Result(void)>                                                                            \
+{                                                                                                            \
+   class yes { char m; };                                                                                    \
+   class no { yes m[2]; };                                                                                   \
+   struct base_mixin                                                                                         \
+   {                                                                                                         \
+     Result member_function_name();                                                                          \
+   };                                                                                                        \
+   struct base : public T, public base_mixin {};                                                             \
+   template <typename U, U t>  class helper{};                                                               \
+   template <typename U>                                                                                     \
+   static no deduce(U*, helper<Result (base_mixin::*)(), &U::member_function_name>* = 0);                    \
+   template <typename U> static yes deduce(...);                                                             \
+public:                                                                                                      \
+   static const bool value = sizeof(yes) == sizeof(deduce<base>(0));                                         \
+};                                                                                                           \
+                                                                                                             \
+template<typename T, typename Result, typename Arg>                                                          \
+class trait_name<T, Result(Arg)>                                                                             \
+{                                                                                                            \
+   class yes { char m; };                                                                                    \
+   class no { yes m[2]; };                                                                                   \
+   struct base_mixin                                                                                         \
+   {                                                                                                         \
+     Result member_function_name(Arg);                                                                       \
+   };                                                                                                        \
+   struct base : public T, public base_mixin {};                                                             \
+   template <typename U, U t>  class helper{};                                                               \
+   template <typename U>                                                                                     \
+   static no deduce(U*, helper<Result (base_mixin::*)(Arg), &U::member_function_name>* = 0);                 \
+   template <typename U> static yes deduce(...);                                                             \
+public:                                                                                                      \
+   static const bool value = sizeof(yes) == sizeof(deduce<base>(0));                                         \
+};                                                                                                           \
+                                                                                                             \
+template<typename T, typename Result, typename Arg1, typename Arg2>                                          \
+class trait_name<T, Result(Arg1,Arg2)>                                                                       \
+{                                                                                                            \
+   class yes { char m; };                                                                                    \
+   class no { yes m[2]; };                                                                                   \
+   struct base_mixin                                                                                         \
+   {                                                                                                         \
+     Result member_function_name(Arg1,Arg2);                                                                 \
+   };                                                                                                        \
+   struct base : public T, public base_mixin {};                                                             \
+   template <typename U, U t>  class helper{};                                                               \
+   template <typename U>                                                                                     \
+   static no deduce(U*, helper<Result (base_mixin::*)(Arg1,Arg2), &U::member_function_name>* = 0);           \
+   template <typename U> static yes deduce(...);                                                             \
+public:                                                                                                      \
+   static const bool value = sizeof(yes) == sizeof(deduce<base>(0));                                         \
+};                                                                                                           \
+                                                                                                             \
+template<typename T, typename Result, typename Arg1, typename Arg2, typename Arg3>                           \
+class trait_name<T, Result(Arg1,Arg2,Arg3)>                                                                  \
+{                                                                                                            \
+   class yes { char m; };                                                                                    \
+   class no { yes m[2]; };                                                                                   \
+   struct base_mixin                                                                                         \
+   {                                                                                                         \
+     Result member_function_name(Arg1,Arg2,Arg3);                                                            \
+   };                                                                                                        \
+   struct base : public T, public base_mixin {};                                                             \
+   template <typename U, U t>  class helper{};                                                               \
+   template <typename U>                                                                                     \
+   static no deduce(U*, helper<Result (base_mixin::*)(Arg1,Arg2,Arg3), &U::member_function_name>* = 0);      \
+   template <typename U> static yes deduce(...);                                                             \
+public:                                                                                                      \
+   static const bool value = sizeof(yes) == sizeof(deduce<base>(0));                                         \
+};                                                                                                           \
+                                                                                                             \
+template<typename T, typename Result, typename Arg1, typename Arg2, typename Arg3, typename Arg4>            \
+class trait_name<T, Result(Arg1,Arg2,Arg3,Arg4)>                                                             \
+{                                                                                                            \
+   class yes { char m; };                                                                                    \
+   class no { yes m[2]; };                                                                                   \
+   struct base_mixin                                                                                         \
+   {                                                                                                         \
+     Result member_function_name(Arg1,Arg2,Arg3,Arg4);                                                       \
+   };                                                                                                        \
+   struct base : public T, public base_mixin {};                                                             \
+   template <typename U, U t>  class helper{};                                                               \
+   template <typename U>                                                                                     \
+   static no deduce(U*, helper<Result (base_mixin::*)(Arg1,Arg2,Arg3,Arg4), &U::member_function_name>* = 0); \
+   template <typename U> static yes deduce(...);                                                             \
+public:                                                                                                      \
+   static const bool value = sizeof(yes) == sizeof(deduce<base>(0));                                         \
+};                                                                                                           
 
 namespace is_call_possible_detail
 { 
@@ -150,6 +224,6 @@ struct trait_name                                                               
    };                                                                                                                   \
                                                                                                                         \
   public:                                                                                                               \
-    static const bool value = impl<trait_name##_detail::has_member<T>::value, Signature>::value; \
+    static const bool value = impl<trait_name##_detail::has_member<T,Signature>::value, Signature>::value;              \
 }; 
 
