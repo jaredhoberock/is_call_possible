0b61746651dfa05a4d465898f1578b4a5683afe4
Jared Hoberock <jaredhoberock@gmail.com>
2012-08-13 18:37:09 -0700

    Tweak DEFINE_HAS_MEMBER_FUNCTION in an effort be a little more portable.

--- is_call_possible.hpp
+++ is_call_possible.hpp
@@ -19,9 +19,9 @@ class trait_name<T, Result(void)>                                               
    template <typename U, U t>  class helper{};                                                               \
    template <typename U>                                                                                     \
    static no deduce(U*, helper<Result (base_mixin::*)(), &U::member_function_name>* = 0);                    \
-   template <typename U> static yes deduce(...);                                                             \
+   static yes deduce(...);                                                                                   \
 public:                                                                                                      \
-   static const bool value = sizeof(yes) == sizeof(deduce<base>(0));                                         \
+   static const bool value = sizeof(yes) == sizeof(deduce(static_cast<base*>(0)));                           \
 };                                                                                                           \
                                                                                                              \
 template<typename T, typename Result, typename Arg>                                                          \
@@ -37,9 +37,9 @@ class trait_name<T, Result(Arg)>                                                
    template <typename U, U t>  class helper{};                                                               \
    template <typename U>                                                                                     \
    static no deduce(U*, helper<Result (base_mixin::*)(Arg), &U::member_function_name>* = 0);                 \
-   template <typename U> static yes deduce(...);                                                             \
+   static yes deduce(...);                                                                                   \
 public:                                                                                                      \
-   static const bool value = sizeof(yes) == sizeof(deduce<base>(0));                                         \
+   static const bool value = sizeof(yes) == sizeof(deduce(static_cast<base*>(0)));                           \
 };                                                                                                           \
                                                                                                              \
 template<typename T, typename Result, typename Arg1, typename Arg2>                                          \
@@ -55,9 +55,9 @@ class trait_name<T, Result(Arg1,Arg2)>                                          
    template <typename U, U t>  class helper{};                                                               \
    template <typename U>                                                                                     \
    static no deduce(U*, helper<Result (base_mixin::*)(Arg1,Arg2), &U::member_function_name>* = 0);           \
-   template <typename U> static yes deduce(...);                                                             \
+   static yes deduce(...);                                                                                   \
 public:                                                                                                      \
-   static const bool value = sizeof(yes) == sizeof(deduce<base>(0));                                         \
+   static const bool value = sizeof(yes) == sizeof(deduce(static_cast<base*>(0)));                           \
 };                                                                                                           \
                                                                                                              \
 template<typename T, typename Result, typename Arg1, typename Arg2, typename Arg3>                           \
@@ -73,9 +73,9 @@ class trait_name<T, Result(Arg1,Arg2,Arg3)>                                     
    template <typename U, U t>  class helper{};                                                               \
    template <typename U>                                                                                     \
    static no deduce(U*, helper<Result (base_mixin::*)(Arg1,Arg2,Arg3), &U::member_function_name>* = 0);      \
-   template <typename U> static yes deduce(...);                                                             \
+   static yes deduce(...);                                                                                   \
 public:                                                                                                      \
-   static const bool value = sizeof(yes) == sizeof(deduce<base>(0));                                         \
+   static const bool value = sizeof(yes) == sizeof(deduce(static_cast<base*>(0)));                           \
 };                                                                                                           \
                                                                                                              \
 template<typename T, typename Result, typename Arg1, typename Arg2, typename Arg3, typename Arg4>            \
@@ -91,9 +91,9 @@ class trait_name<T, Result(Arg1,Arg2,Arg3,Arg4)>                                
    template <typename U, U t>  class helper{};                                                               \
    template <typename U>                                                                                     \
    static no deduce(U*, helper<Result (base_mixin::*)(Arg1,Arg2,Arg3,Arg4), &U::member_function_name>* = 0); \
-   template <typename U> static yes deduce(...);                                                             \
+   static yes deduce(...);                                                                                   \
 public:                                                                                                      \
-   static const bool value = sizeof(yes) == sizeof(deduce<base>(0));                                         \
+   static const bool value = sizeof(yes) == sizeof(deduce(static_cast<base*>(0)));                           \
 };                                                                                                           
 
 namespace is_call_possible_detail
